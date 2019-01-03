df1fcb71529fe9622f9516fd3450951f3bee9b4d
Falko Schumann <falko.schumann@muspellheim.de>
2019-01-03 20:43:33 +0100

    Implement recursive alternative algorithm

--- src/main/java/de/muspellheim/roman/RomanConverter.java
+++ src/main/java/de/muspellheim/roman/RomanConverter.java
@@ -1,61 +1,31 @@
 package de.muspellheim.roman;
 
-import java.text.*;
-import java.util.stream.*;
+import java.util.*;
 
 public class RomanConverter {
 
-    public static int fromRoman(String romanNumeral) throws ParseException {
-        var numbers = new Parser().parse(romanNumeral);
-        numbers = subtract(numbers);
-        return numbers.sum();
-    }
+    private static final Map<Character, Integer> DIGITS = Map.of(
+            'I', 1,
+            'V', 5,
+            'X', 10,
+            'L', 50,
+            'C', 100,
+            'D', 500,
+            'M', 1000
+    );
 
-    private static IntStream subtract(IntStream numbers) {
-        var a = numbers.toArray();
-        for (int i = 0; i < a.length - 1; i++) {
-            if (a[i] < a[i + 1]) {
-                a[i] *= -1;
-            }
-        }
-        return IntStream.of(a);
-    }
-
-    private static class Parser {
-
-        private int index;
-
-        private IntStream parse(String text) throws ParseException {
-            var builder = IntStream.builder();
-            for (index = 0; index < text.length(); index++) {
-                char c = text.charAt(index);
-                int n = mapFromRoman(c);
-                builder.accept(n);
-            }
-            return builder.build();
+    public static int fromRoman(String romanNumeral) {
+        int currentDigit = DIGITS.get(romanNumeral.charAt(0));
+        if (romanNumeral.length() == 1) {
+            return currentDigit;
         }
 
-        private int mapFromRoman(char c) throws ParseException {
-            switch (c) {
-                case 'I':
-                    return 1;
-                case 'V':
-                    return 5;
-                case 'X':
-                    return 10;
-                case 'L':
-                    return 50;
-                case 'C':
-                    return 100;
-                case 'D':
-                    return 500;
-                case 'M':
-                    return 1000;
-                default:
-                    throw new ParseException("not a roman digit: " + c, index);
-            }
+        int nextDigit = DIGITS.get(romanNumeral.charAt(1));
+        if (currentDigit < nextDigit) {
+            currentDigit *= -1;
         }
 
+        return currentDigit + fromRoman(romanNumeral.substring(1));
     }
 
 }
--- src/test/java/de/muspellheim/roman/RomanConverterTests.java
+++ src/test/java/de/muspellheim/roman/RomanConverterTests.java
@@ -8,32 +8,18 @@ import java.text.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Tests for roman numerals converter")
 class RomanConverterTests {
 
-    @Test
-    void convert() throws ParseException {
-        assertEquals(4, RomanConverter.fromRoman("IV"));
-    }
-
-    @ParameterizedTest(name = "[{index}] Convert from roman numeral {0} to {1}")
+    @ParameterizedTest(name = "[{index}] Convert from roman numeral {0} to arabic numeral {1}")
     @CsvSource({
             "M, 1000", // nur eine römische Ziffer
             "XVI, 16", // nur Addition von Ziffernwerten
             "MCDXCII, 1492",
             "MCMLXXXIV, 1984",
     })
-    void fromRomanNumerals(String romanNumeral, int translation) throws ParseException {
-        assertEquals(translation, RomanConverter.fromRoman(romanNumeral));
-    }
-
-    @Test
-    @DisplayName("XAI is not a roman numeral")
-    void notARomanNumeral() {
-        ParseException exception = assertThrows(ParseException.class, () -> {
-            RomanConverter.fromRoman("XAI");
-        });
-        assertEquals("not a roman digit: A", exception.getMessage());
-        assertEquals(1, exception.getErrorOffset());
+    void convertFromRomanNumeralToArabicNueral(String romanNumeral, int arabicNumeral) throws ParseException {
+        assertEquals(arabicNumeral, RomanConverter.fromRoman(romanNumeral));
     }
 
 }
